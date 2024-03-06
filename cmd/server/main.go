1967ce447ec748b02c240a5486ad195b4dd40dfa
Jaeiya <exanix@gmail.com>
2024-03-06 15:14:39 -0800

    feat(main): print useful config info

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/Everything-Explained/go-server/internal/lib"
 	"github.com/Everything-Explained/go-server/internal/utils"
 )
 
 func main() {
-	fmt.Printf("InDev: %v\nConfigFilePath: %s\n", utils.Env.InDev, utils.Env.ConfigFilePath)
+	config := lib.GetConfig()
+	fmt.Printf(
+		"InDev: %v\nConfig: %s\nPort: %d\nMail: %s\n",
+		utils.Env.InDev,
+		utils.Env.ConfigFilePath,
+		config.Port,
+		config.Mail.Host,
+	)
 	http.ListenAndServe("127.0.0.1:8080", nil)
 }
