0b756d6ffa675535f4b5ee73732134d8a72e4303
Jaeiya <exanix@gmail.com>
2024-03-07 19:03:49 -0800

    feat(responsewriter): add boolean store support

--- internal/router/http_interface/response_writer.go
+++ internal/router/http_interface/response_writer.go
@@ -3,15 +3,17 @@ package http_interface
 import "net/http"
 
 const (
-	maxIntStoreSize = 10
-	maxStrStoreSize = 10
+	maxIntStoreSize  = 10
+	maxStrStoreSize  = 10
+	maxBoolStoreSize = 10
 )
 
 type ResponseWriter struct {
 	http.ResponseWriter
-	strStore map[string]string
-	intStore map[string]int64
-	status   int
+	strStore  map[string]string
+	intStore  map[string]int64
+	boolStore map[string]bool
+	status    int
 }
 
 func (rw *ResponseWriter) StoreStr(id string, val string) string {
@@ -24,12 +26,21 @@ func (rw *ResponseWriter) StoreInt(id string, val int64) int64 {
 	return val
 }
 
+func (rw *ResponseWriter) StoreBool(id string, val bool) bool {
+	rw.boolStore[id] = val
+	return val
+}
+
 func (rw *ResponseWriter) GetInt(id string) int64 {
 	return rw.intStore[id]
 }
 
 func (rw *ResponseWriter) GetStr(id string) string {
 	return rw.strStore[id]
+}
+
+func (rw *ResponseWriter) GetBool(id string) bool {
+	return rw.boolStore[id]
 }
 
 func (rw *ResponseWriter) GetStatus() int {
@@ -46,5 +57,6 @@ func CreateResponseWriter(rw http.ResponseWriter) *ResponseWriter {
 		ResponseWriter: rw,
 		strStore:       make(map[string]string, maxStrStoreSize),
 		intStore:       make(map[string]int64, maxIntStoreSize),
+		boolStore:      make(map[string]bool, maxBoolStoreSize),
 	}
 }
