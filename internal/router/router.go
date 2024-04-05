Everything-Explained/go-server

commit 246bddd502b4dac2e8b078e3b0ab7f480b6a51d6

fix(router): add timeout for server write/read ops

internal/router/router.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/Everything-Explained/go-server/internal"
 )
@@ -112,7 +113,12 @@ func (r *Router) SetStaticRoute(
 }
 
 func (r *Router) ListenAndServe(addr string, port int) error {
-	return http.ListenAndServe(fmt.Sprintf("%s:%d", addr, port), r.Handler)
+	s := http.Server{
+		Addr:         fmt.Sprintf("%s:%d", addr, port),
+		ReadTimeout:  8 * time.Second,
+		WriteTimeout: 8 * time.Second,
+	}
+	return s.ListenAndServe()
 }
 
 func GetContextValue[T any](key any, r *http.Request) (T, error) {

.golangci.yml
@@ -1,6 +1,10 @@
 linters-settings:
   nestif:
     min-complexity: 5
+  gosec:
+    exclude-generated: false
+    severity: low
+    confidence: low
   revive:
     max-open-files: 2048
     ignore-generated-header: true

commit 8a25df97768db028ee2e3eb8909af8a8dc62338d

fix(router): missing handler
This is a regression from 246bddd502b4dac2e8b078e3b0ab7f480b6a51d6

internal/router/router.go
@@ -112,11 +112,12 @@ func (r *Router) SetStaticRoute(
 	}, mw...)
 }
 
-func (*Router) ListenAndServe(addr string, port int) error {
+func (r *Router) ListenAndServe(addr string, port int) error {
 	s := http.Server{
 		Addr:         fmt.Sprintf("%s:%d", addr, port),
 		ReadTimeout:  8 * time.Second,
 		WriteTimeout: 8 * time.Second,
+		Handler:      r.Handler,
 	}
 	return s.ListenAndServe()
 }