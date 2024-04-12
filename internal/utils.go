4c92813e4e6cee823a5db843b26467250345e0c1
Jaeiya <exanix@gmail.com>
2024-04-12 11:01:18 -0700

    feat(utils): add error print helpers for tests

--- internal/utils.go
+++ internal/utils.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"sync"
@@ -35,6 +36,19 @@ func GetWorkingDir() string {
 		workingDir = wd
 	})
 	return workingDir
+}
+
+// PrintErrorS returns a string formatted for simple test errors
+func PrintErrorS(want any, got any) string {
+	return fmt.Sprintf("\n\t(WANT= %v) \n\t(GOT= %v)", want, got)
+}
+
+/*
+PrintErrorD returns a string formatted for descriptive test errors,
+allowing for an expectation to describe what should be happening.
+*/
+func PrintErrorD(expected string, want any, got any) string {
+	return fmt.Sprintf("\n\t(EXPECTED= %s) %s", expected, PrintErrorS(want, got))
 }
 
 func GetISODateNow() string {
