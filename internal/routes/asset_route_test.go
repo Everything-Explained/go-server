e30b95ef3722d302928a3b3e1d9f4974c7f749d5
Jaeiya <exanix@gmail.com>
2024-04-13 14:07:42 -0700

    tdd: add asset route test
    ref: $124

---
+++ internal/routes/asset_route_test.go
@@ -0,0 +1,37 @@
+package routes
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/Everything-Explained/go-server/internal/router"
+	"github.com/Everything-Explained/go-server/testutils"
+)
+
+func TestAssetRoute(t *testing.T) {
+	os.Chdir("../../../go-server")
+	t.Parallel()
+
+	r := router.NewRouter()
+	HandleAssets(r, "./mocks")
+
+	rec := testutils.MockRequest(r.Handler, "GET", "/assets/mock.txt", nil)
+
+	t.Run("status 200", func(t *testing.T) {
+		if rec.Code != http.StatusOK {
+			t.Error(testutils.PrintErrorS(http.StatusOK, rec.Code))
+		}
+	})
+
+	t.Run("returns file", func(t *testing.T) {
+		d, _ := io.ReadAll(rec.Body)
+		gotBody := string(d)
+		wantBody := "test text"
+
+		if gotBody != wantBody {
+			t.Error(testutils.PrintErrorS(wantBody, gotBody))
+		}
+	})
+}
