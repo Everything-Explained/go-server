40a9105530cfd41baf5fcf2659fb6f01130a60a3
Jaeiya <exanix@gmail.com>
2024-04-23 09:03:48 -0700

    tdd(assetroute): use testify library

--- internal/routes/asset_route_test.go
+++ internal/routes/asset_route_test.go
@@ -1,47 +1,33 @@
 package routes
 
 import (
-	"io"
 	"net/http"
 	"os"
 	"testing"
 
 	"github.com/Everything-Explained/go-server/internal/router"
 	"github.com/Everything-Explained/go-server/testutils"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestAssetRoute(t *testing.T) {
 	t.Parallel()
-	dir := t.TempDir()
+	assert := assert.New(t)
+	rq := require.New(t)
 
+	dir := t.TempDir()
 	err := os.Mkdir(dir+"/mocks", 0o644)
-	if err != nil {
-		t.Fatal(err)
-	}
+	rq.NoError(err, "create mock dir")
 
 	err = os.WriteFile(dir+"/mocks/mock.txt", []byte("test text"), 0o600)
-	if err != nil {
-		t.Fatal(err)
-	}
+	rq.NoError(err, "create mock file")
 
 	r := router.NewRouter()
 	HandleAssets(r, dir+"/mocks")
 
 	rec := testutils.MockRequest(r.Handler, "GET", "/assets/mock.txt", nil)
 
-	t.Run("status 200", func(t *testing.T) {
-		if rec.Code != http.StatusOK {
-			t.Error(testutils.PrintErrorS(http.StatusOK, rec.Code))
-		}
-	})
-
-	t.Run("returns file", func(t *testing.T) {
-		d, _ := io.ReadAll(rec.Body)
-		gotBody := string(d)
-		wantBody := "test text"
-
-		if gotBody != wantBody {
-			t.Error(testutils.PrintErrorS(wantBody, gotBody))
-		}
-	})
+	rq.Equal(http.StatusOK, rec.Code, "expect StatusOk")
+	assert.Equal("test text", rec.Body.String(), "returns expected body text")
 }
